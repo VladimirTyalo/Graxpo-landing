84441d9daf76a5f3a7604db4268b88014496524f
Vladimir Tyalo <bobtial@inbox.ru>
2016-12-21 01:18:06 +0400

    feature(service-card)

--- tasks/icons.js
+++ tasks/icons.js
@@ -5,20 +5,25 @@ import rename from 'gulp-rename';
 import plumber from 'gulp-plumber';
 import errorHandler from 'gulp-plumber-error-handler';
 import path from 'path';
+import rsp from 'remove-svg-properties';
 
 gulp.task('icons', () => (
-	gulp.src('app/icons/**/*.svg')
-		.pipe(plumber({errorHandler: errorHandler(`Error in 'icons' task`)}))
-		.pipe(svgSymbols({
-			title: false,
-			id: 'icon_%f',
-			className: '%f',
-			templates: [
-				path.join(__dirname, '../node_modules/stylus-svg-size-template/svg-size.styl'),
-				'default-svg'
-			]
-		}))
-		.pipe(gulpIf(/\.styl$/, gulp.dest('app/styles/helpers')))
-		.pipe(gulpIf(/\.svg$/, rename('icon.svg')))
-		.pipe(gulpIf(/\.svg$/, gulp.dest('dist/assets/images/')))
+    gulp.src('app/icons/**/*.svg')
+    .pipe(plumber({ errorHandler: errorHandler(`Error in 'icons' task`) }))
+    .pipe(rsp.stream.remove({
+        // properties: ['fill', 'stroke', 'color']
+    }))
+    .pipe(svgSymbols({
+        title: false,
+        id: 'icon_%f',
+        className: '%f',
+        templates: [
+            path.join(__dirname, '../node_modules/stylus-svg-size-template/svg-size.styl'),
+            'default-svg'
+        ]
+    }))
+    .pipe(gulpIf(/\.styl$/, gulp.dest('app/styles/helpers')))
+    .pipe(gulpIf(/\.svg$/, rename('icon.svg')))
+    .pipe(gulpIf(/\.svg$/, gulp.dest('dist/assets/images/')))
+    .pipe(gulpIf(/\.svg$/, gulp.dest('app/blocks/icon/')))
 ));
